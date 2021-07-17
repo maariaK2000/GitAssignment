2d85de3271af88be75c341be176ee56f3b762e0a
maariaK2000 <maariak530@gmail.com>
2021-07-17 12:18:13 +0530

    West part removed

--- Location.java
+++ Location.java
@@ -1,5 +1,5 @@
 public class Location{
       public static void main(String[] args){
-            System.out.println("I live in Kurla West behind school");
+            System.out.println("I live in Kurla behind school");
       }
 }
